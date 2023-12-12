f860da5a7bd12096124637699b633c2c9f45458e
Scott Coupar <scott.coupar@googlemail.com>
2023-12-12 23:18:47 +0000

    final styles

--- src/app/restaurant/setup/GoogleBooking/page.tsx
+++ src/app/restaurant/setup/GoogleBooking/page.tsx
@@ -7,7 +7,7 @@ export default function simpleERB() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8  mt-10 mb-20 py-4">
         <div className="flex mx-8 justify-between">
           <h1 className="text-2xl font-bold leading-7">Google Booking</h1>
-          <a className="backbtn inline-flex items-center px-4 border border-gray-300 shadow-sm text-base font-medium rounded-md hover:bg-gray-50 active:bg-gray-50 active:text=gray-800 focus:outline-none">
+          <a className="backbtn inline-flex items-center px-4 py-2 border border-gray-300 shadow-sm text-base font-medium rounded-md hover:bg-gray-50 active:bg-gray-50 active:text=gray-800 focus:outline-none cursor-pointer">
             Back
           </a>
         </div>
